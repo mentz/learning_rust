265fdb1c9ac189e5837b6a9e32dd1ad8e2318148
Lucas Litter Mentz <4259237+mentz@users.noreply.github.com>
2025-03-26 21:39:38 -0300

    solve 06_move_semantics and 7_structs

--- rustlings/exercises/06_move_semantics/move_semantics2.rs
+++ rustlings/exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,7 @@
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
-    vec.push(88);
-
-    vec
+    let mut vec2 = vec;
+    vec2.push(88);
+    vec2
 }
 
 fn main() {
@@ -19,8 +17,7 @@ mod tests {
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
-
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
